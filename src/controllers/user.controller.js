172f4269b2924d25e79636123946f439ebad9395
YK Chaudhari <155022241+YashChaudhari1805@users.noreply.github.com>
2026-03-14 17:24:48 +0530

    Added controls

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,43 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { apiError } from '../utils/apiError.js'
+import { User } from '../models/user.model.js'
+import { apiResponse } from '../utils/apiResponse.js'
 
-const registerUser = asyncHandler(async(req,res)=> {
-    res.status(200).json({
-        message: 'ok'
+const registerUser = asyncHandler(async (req, res) => {
+    const { userName, email, password, age, height, weight, goal } = req.body
+
+    if ([userName, email, password, age, height, weight, goal].some((field) => field?.trim() === "")) {
+        throw new apiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ userName }, { email }]
     })
+
+    if (existedUser) {
+        throw new apiError(409, "User already exists")
+    }
+
+    User.create({
+        userName,
+        email,
+        password,
+        age,
+        height,
+        weight,
+        goal
+    })
+
+    const createdUser = await User.findById(User._id).select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new apiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new apiResponse(200, createdUser, "User Registered Successfully!")
+    )
+
 })
 
-export {registerUser}
+export { registerUser }
