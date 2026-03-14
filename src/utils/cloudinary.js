ce289c9cc0eb4761ccb8aafc87c24d4695722c01
YK Chaudhari <155022241+YashChaudhari1805@users.noreply.github.com>
2026-03-14 14:42:14 +0530

    Added Cloudinary and Multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const uploadOnCloudinary = async (localfilepath) => {
     try {
         if (!localfilepath) return null
-        const reponse = await cloudinary.uploader.upload
+        const response = await cloudinary.uploader.upload
             (localfilepath, {
                 resource_type: "auto"
             })
